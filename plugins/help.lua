6028453b7acfd28065ee63e7e6d1b7c7873323e6
ADVAN <mahdijanlou@gmail.com>
2016-06-11 11:15:48 +0430

    Source has been fixed!

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,13 +1,13 @@
 function help(msg, matches)
 
     if is_realm(msg) and is_sudo(msg) then
-    	HelpRealm = io.open("./helps/HelpRealm.txt, ok_cb, false)
+    	HelpRealm = io.open("./helps/HelpRealm.txt", ok_cb, false)
     	help_text_realm = HelpRealm:read("*all")
 	  return help_text_realm
 	end
 	
 	if is_group(msg) and is_momod(msg) or is_owner(msg) then
-	HelpChat = io.open("./helps/HelpChat.txt, ok_cb, false)
+	HelpChat = io.open("./helps/HelpChat.txt", ok_cb, false)
     	help_text = HelpChat:read("*all")
 	  return help_text
 	elseif is_group(msg) and not is_momod(msg) or not is_owner(msg) then
@@ -15,7 +15,7 @@ function help(msg, matches)
 	end
 	
 	if is_super_group(msg) and is_momod(msg) or is_owner(msg) then
-	HelpSuper = io.open("./helps/HelpSuper.txt, ok_cb, false)
+	HelpSuper = io.open("./helps/HelpSuper.txt", ok_cb, false)
     	help_text_super = HelpSuper:read("*all")
 	  return help_text_super
 	elseif is_super_group(msg) and not is_momod(msg) or not is_owner(msg) then
