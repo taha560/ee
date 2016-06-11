676c526a5b293627e9b379574cde43d3f8da03e6
ADVAN <mahdijanlou@gmail.com>
2016-06-11 12:17:35 +0430

    fixed!

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,26 +1,28 @@
-function help(msg, matches)
-
+function run(msg, matches)
+	
     if is_realm(msg) and is_sudo(msg) then
-    	HelpRealm = io.open("./helps/HelpRealm.txt", ok_cb, false)
+   	HelpRealm = io.open("./helps/HelpRealm.txt", "r")
     	help_text_realm = HelpRealm:read("*all")
 	  return help_text_realm
 	end
+
+if is_momod(msg) or is_owner(msg) or is_sudo(msg) then
 	
-	if is_group(msg) and is_momod(msg) or is_owner(msg) then
-	HelpChat = io.open("./helps/HelpChat.txt", ok_cb, false)
-    	help_text = HelpChat:read("*all")
-	  return help_text
-	elseif is_group(msg) and not is_momod(msg) or not is_owner(msg) then
-	  return "You can see /help text"
-	end
-	
-	if is_super_group(msg) and is_momod(msg) or is_owner(msg) then
-	HelpSuper = io.open("./helps/HelpSuper.txt", ok_cb, false)
+	if msg.to.type == 'channel' then
+	HelpSuper = io.open("./helps/HelpSuper.txt", "r")
     	help_text_super = HelpSuper:read("*all")
 	  return help_text_super
-	elseif is_super_group(msg) and not is_momod(msg) or not is_owner(msg) then
-	  return "You can see /help text"
 	end
+	
+    if msg.to.type == 'chat' then
+	HelpChat = io.open("./helps/HelpChat.txt", "r")
+    	help_text = HelpChat:read("*all")
+	  return help_text
+	end
+	
+ elseif not is_momod(msg) or not is_owner(msg) then
+	  return "You cant see /help text"
+end
 	
 	if is_support(msg) then
 	  return "This is support"
@@ -29,5 +31,5 @@ end
 return {
 advan = "http://github.com/janlou",
 patterns = {"^[!#/](help)$"},
-help = run,
+run = run,
 }
